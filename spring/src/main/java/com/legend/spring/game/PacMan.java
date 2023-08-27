7f2d745ea46961243a2a48b055fdcd71a0b2dc46
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2023-08-27 20:47:07 +0530

    learned what is the use of @Component and @ComponentScan and Applied @Component on Pacman and GameRuuner And @ComponentScan on The GameLauncherApplication (Configuration Class)

--- spring/src/main/java/com/legend/spring/game/PacMan.java
+++ spring/src/main/java/com/legend/spring/game/PacMan.java
@@ -1,5 +1,11 @@
 package com.legend.spring.game;
 
+import org.springframework.stereotype.Component;
+
+// Add the @Component annotation to the class to make it a Spring bean.
+// Save the file and ensure that the class is within the component scan path of your Spring application.
+// This allows Spring to detect and manage the bean.
+@Component
 public class PacMan implements GamingConsole{
     @Override
     public void up() {
---
+++ spring/src/main/java/com/legend/spring/learnspringframework/GamingAppLauncherApplication.java
@@ -0,0 +1,33 @@
+package com.legend.spring.learnspringframework;
+
+import com.legend.spring.game.GameRunner;
+import com.legend.spring.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*
+The @ComponentScan annotation is used in Spring to specify the packages that should be scanned for Spring components, such as beans, controllers, services, etc.
+When you use @ComponentScan("com.legend.spring.game"),
+it tells Spring to scan the com.legend.spring.game package and its sub-packages for components.
+ */
+@Configuration
+@ComponentScan("com.legend.spring.game")
+public class GamingAppLauncherApplication {
+
+    // Created a GameRunner
+    // and wired PacMan with GameRunner with the help of Parameter
+
+    // We have used @Component to create the bean for the PacMan and GameRunner
+    // @ComponentScan is used to tell Spring, which package to scan for the component
+
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
+            context.getBean(GamingConsole.class).up();
+
+            context.getBean(GameRunner.class).run();
+        }
+
+    }
+}
