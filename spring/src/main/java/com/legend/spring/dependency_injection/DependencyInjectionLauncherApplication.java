8c65b06540fb277adf5d3f1892832916c8603707
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2023-08-28 00:16:27 +0530

    Learned Setter injection In Spring FrameWork

--- spring/src/main/java/com/legend/spring/dependency_injection/DependencyInjectionLauncherApplication.java
+++ spring/src/main/java/com/legend/spring/dependency_injection/DependencyInjectionLauncherApplication.java
@@ -19,12 +19,30 @@ class YourBusinessClass {
     // This is called field injection
     // As soon as we put autowired on a field
     // Spring Automatically do field Injection
-     @Autowired
-     Dependency1 dependency1;
+//     @Autowired
+//     Dependency1 dependency1;
+//
+//     @Autowired
+//     Dependency2 dependency2;
 
-     @Autowired
-     Dependency2 dependency2;
 
+//    Now we will Learn Setter Dependency Injection
+//    Done through Setters
+    Dependency1 dependency1;
+
+    Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Dependency Injection :- setDependency 1");
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Dependency Injection :- setDependency 2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
