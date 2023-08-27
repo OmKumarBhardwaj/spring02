fb5b7093cf3591b2ba1b2820396794f55335f3df
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2023-08-27 17:32:02 +0530

    Deleted Configuration file from the project and moved the Beans to App03SpringBean Main class

--- spring/src/main/java/com/legend/spring/learnspringframework/App03SpringBean.java
+++ spring/src/main/java/com/legend/spring/learnspringframework/App03SpringBean.java
@@ -3,16 +3,28 @@ package com.legend.spring.learnspringframework;
 import com.legend.spring.game.GameRunner;
 import com.legend.spring.game.GamingConsole;
 import com.legend.spring.game.PacMan;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import static org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt.AnnotationConfigApplicationContext;
-
-@SpringBootApplication
+@Configuration
 public class App03SpringBean {
+
+    @Bean
+    public GamingConsole game() {
+        return new PacMan();
+    }
+
+    // Created a GameRunner
+    // and wired PacMan with GameRunner with the help of Parameter
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+
     public static void main(String[] args) {
 
-        try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(App03SpringBean.class)) {
             context.getBean(GamingConsole.class).up();
 
             context.getBean(GameRunner.class).run();
